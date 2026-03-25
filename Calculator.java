136a481f95d85b60cc6df35a517ef74af8f4a7ce
Vedansh choubey <vedanshchoubeyvedu2006@gmail.com>
2026-03-25 23:47:12 +0530

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Calculator {
+    
+    public static double add(double a, double b) {
+        return a + b;
+    }
+    
+    public static double subtract(double a, double b) {
+        return a - b;
+    }
+    
+    public static double multiply(double a, double b) {
+        return a * b;
+    }
+    
+    public static double divide(double a, double b) {
+        if (b == 0) {
+            System.out.println("Error: Division by zero!");
+            return 0;
+        }
+        return a / b;
+    }
